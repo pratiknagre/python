f7162afe5e5b77e7f1a047ee1285ccbfdae9ae40
N1 <pathaknishant003@gmail.com>
2023-06-06 10:44:04 -0400

    added

---
+++ report2.py
@@ -0,0 +1,31 @@
+{DATA-STRUCTURE}=======>
+
+In Python, a data structure is a way of organizing and storing data in a specific format. Python provides several built-in data structures that are commonly used to store and manipulate collections of related data. Some of the commonly used data structures in Python are:
+
+List (list): An ordered collection of elements that can be of different types. Lists are mutable, which means their elements can be modified.
+
+Tuple (tuple): Similar to a list, but tuples are immutable, meaning their elements cannot be modified once defined.
+
+Set (set): An unordered collection of unique elements. Sets do not allow duplicate values.
+
+Dictionary (dict): A collection of key-value pairs, where each key is unique. Dictionaries provide efficient lookup and retrieval of values based on their keys.
+
+String (str): Represents a sequence of characters. Strings are immutable.
+
+These built-in data structures offer different characteristics and are suitable for different use cases. Python also provides additional data structures through modules and libraries. Some commonly used data structures from modules include:
+
+Arrays (array): A collection of elements of the same type, stored in contiguous memory locations. Arrays offer efficient storage and retrieval of elements.
+
+Queue (queue): A data structure that follows the First-In-First-Out (FIFO) principle. Elements are inserted at one end and removed from the other end.
+
+Stack (stack): A data structure that follows the Last-In-First-Out (LIFO) principle. Elements are inserted and removed from the same end.
+
+Linked List (linked list): A data structure where elements are stored in nodes, and each node contains a reference to the next node.
+
+Tree (tree): A hierarchical data structure with nodes connected by edges. Trees are widely used in algorithms and data organization.
+
+Graph (graph): A collection of nodes (vertices) connected by edges. Graphs are used to represent complex relationships and networks.
+
+Python also allows you to create custom data structures using classes and objects, allowing you to define data structures tailored to your specific needs.
+
+These data structures provide different ways to store, organize, and manipulate data based on the requirements of your program. Choosing the appropriate data structure is crucial for efficient and effective programming.
